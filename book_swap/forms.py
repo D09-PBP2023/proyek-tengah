962945873c6d87aa66fce4404f2c8ebe4525c6ad
gilangp03 <gilangfajarp.03@gmail.com>
2023-10-29 23:21:43 +0700

    feat:fix database 3rd

--- book_swap/forms.py
+++ book_swap/forms.py
@@ -1,17 +1,7 @@
 from django.forms import ModelForm
 from book_swap.models import BookSwap
-from main.models import Book
-from django import forms    
 
 class BookSwapForm(ModelForm):
-    bookslib = Book.objects.all()
-    book_choose_list = []
-    count = 0
-    for book in bookslib:
-        book_choose_list.append((book.name, book.name))
-        book_choose = tuple(book_choose_list)
-
-    name = forms.CharField(widget=forms.Select(choices=book_choose))    
     class Meta:
         model = BookSwap
         fields = ["name", "message"]
