07e0fce887c2a1d768525879c835e420fd7506a4
rxa15 <114054405+rxa15@users.noreply.github.com>
2023-10-29 11:50:25 +0700

    Push model

--- book_request/models.py
+++ book_request/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+from main.models import Book
+
+class RequestBook(Book):
+    PENDING = 'PENDING'
+    APPROVED = 'APPROVED'
+    REJECTED = 'REJECTED'
+
+    STATUS_CHOICES = [
+        (PENDING, 'Pending'),
+        (APPROVED, 'Approved'),
+        (REJECTED, 'Rejected'),
+    ]
+    
+    request_date_added = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    request_status = models.CharField(max_length=8, choices=STATUS_CHOICES, default=PENDING)
--- book_request/views.py
+++ book_request/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.core import serializers
+from .models import Book
+from django.views.decorators.csrf import csrf_exempt
+from book_request.forms import RequestBookForm
 
-# Create your views here.
+@csrf_exempt
+def request_book_by_ajax(request):
+    if request.method == 'POST':
+        form = RequestBookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'error': False, 'message': 'Request Added Successfully'})
+        else:
+            return JsonResponse({'error': True, 'errors': form.errors, 'message': 'Request Failed'})
+    else:
+        form = RequestBookForm()
+        return render(request, 'book_request_by_ajax.html', {'form': form})
+
+def get_book_json(request):
+    book = Book.objects.all()
+    return HttpResponse(serializers.serialize('json', book))
