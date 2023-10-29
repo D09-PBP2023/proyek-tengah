be69c3702ceeacbca14ee5664dcef7fb4a68e36a
MRadhityaUtomo <radhityautomo26@gmail.com>
2023-10-29 23:33:47 +0700

    fixed stufff

--- user_profile/views.py
+++ user_profile/views.py
@@ -118,3 +118,15 @@ def book_list(request):
         'bmarkedbooks': bmarkedbooks,
     }
     return render(request, 'book_list.html', context)
+
+
+
+
+def get_username_by_id(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    username = user.username
+    
+    context = {
+        'username': username,
+    }
+    return render(request, 'username_detail.html', context)
