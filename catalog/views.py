1b1879ca73b20a706f77fc3c43fff1d12d2261ae
marvelm57 <124948653+marvelm57@users.noreply.github.com>
2023-12-18 20:54:08 +0700

    Update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -7,6 +7,7 @@ from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+import json
 
 # Create your views here.
 
