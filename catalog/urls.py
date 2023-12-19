fefd5f8b55256561ba805b7b3f49c441fd87a4c2
marvelm57 <124948653+marvelm57@users.noreply.github.com>
2023-12-19 16:53:33 +0700

    Feature/review (#13)
    * added features for viewing profile (raw)
    
    * Interface mentahan book details
    
    * fixed integrity errors on models
    
    * Penambahan modul bookmarked pada app user_profile dan catalog
    
    * Penambahan modul bookmarked pada app user_profile dan catalog
    
    * fix: retrieve back removed migration files
    
    * added final UI to user profile
    
    * redo bookmark
    
    * added final UI to user profile
    
    * feat: integrate homepage with detail book
    
    * Added UI and features for editing user profile
    
    * Make new branch for book request management
    
    * Added default profile picture, buttons for connecting to bookmarked pages
    
    * visual bookmarked list
    
    * Added placeholder book covers when there are no favorited books
    
    * Added placeholder book covers when there are no favorited books-v2, fixed height error
    
    * penyelesaian fitur bookmarked dengan button AJAX
    
    * feat: responsive styling improvement
    
    * Revise model
    
    * Push model
    
    * Testing push files
    
    * Added comment to html
    
    * implemented add favorite book ajax feature
    
    * for fetching
    
    * merged with main
    
    * feat review
    
    * menjadikan UI responsive untuk screen size diatas 320px
    
    * add static default user profile picture
    
    * menjadikan UI responsive untuk screen size diatas 320px- fixed conflicting issues
    
    * Feature/testing django (#5)
    
    * feat: add test for book model/view
    
    * feat: split header to own file
    
    * feat: add django test for main, catalog, users
    
    * feat: add book model to admin
    
    * feat: hide bookmark buttons when not authenticated
    
    * feat: split header to own component
    
    * feat: add deployment
    
    * fix: typo name project on deployment
    
    * feat: run test deployment
    
    * fix: testing deployment
    
    * feat: add dependency testing before deployment
    
    * menjadikan UI responsive untuk screen size diatas 320px- fixed conflicting issues -v3, fixed more conflicting issues
    
    * added function to return username from user id
    
    * fix: rating on main page
    
    * fix: remove typecheckingmode
    
    * Finish making book request management platform
    
    * fixed stufff
    
    * UI FIX PLEASSEEE
    
    * UI FIX PLEASSEEE 2
    
    * fixed userprofile methods, broken userprofilepage
    
    * update views.py, add method create review flutter
    
    * update urls.py, add url path to create review flutter
    
    * ngambil data userprofile udh bisa -maap tadi ada sesuatu ketinggalan- maap
    
    * Add bookmark for flutter
    
    * fix bug (inconsistent bookmarking)
    
    * test
    
    * profile udah, tinggal edit fav books
    
    * FINALLY
    
    * migrate ketinggalan
    
    * Update views.py
    
    * resolve conflicts
    
    * update views.py to create review flutter
    
    * resolve conflict
    
    * resolve conflicts
    
    * resolve conflicts
    
    * fix: type error on python
    
    ---------
    
    Co-authored-by: MRadhityaUtomo <radhityautomo26@gmail.com>
    Co-authored-by: eilalleinat <eilalleinat@gmail.com>
    Co-authored-by: ArvinCS <arvinciu86@yahoo.com>
    Co-authored-by: laras malahayati <114054405+rxa15@users.noreply.github.com>
    Co-authored-by: Arvin <36118285+ArvinCS@users.noreply.github.com>

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from catalog.views import book_details, bookmark_book, bookmark_book_flutter, get_marked_books, get_review, add_review, get_average_rating
+from catalog.views import book_details, bookmark_book, bookmark_book_flutter, get_marked_books, get_review, add_review, get_average_rating, create_review_flutter
 from user_profile.views import view_bookmarked_list
 
 app_name = 'catalog'
@@ -13,5 +13,6 @@ urlpatterns = [
     path('get_review/<int:id>/', get_review, name='get_review'),
     path('add_review/<int:id>/', add_review, name='add_review'),
     path('get_average_rating/<int:id>/', get_average_rating, name='get_average_rating'),
+    path('add-review-flutter/', create_review_flutter, name='create_review_flutter'),
     path('bookmarkflutter/<int:id>/', bookmark_book_flutter, name='bookmark_book_flutter'),
 ]
