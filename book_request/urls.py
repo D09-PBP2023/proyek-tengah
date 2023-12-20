4e3811ebf60c6724b854f800a472b631e1bf6c3b
rxa15 <114054405+rxa15@users.noreply.github.com>
2023-12-20 09:48:18 +0700

    Push sementara untuk fixing

--- book_request/urls.py
+++ book_request/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from book_request.views import request_book_by_ajax, get_requested_book_json, view_all_requests, approve_request, reject_request
+from book_request.views import request_book_by_ajax, get_requested_book_json, view_all_requests, approve_request, reject_request, request_book_mobile
 
 app_name = 'book_request'
 
@@ -9,4 +9,10 @@ urlpatterns = [
     path('requests/', view_all_requests, name='view_all_requests'),
     path('requests/approve/<int:request_id>', approve_request, name='approve_request'),
     path('requests/reject/<int:request_id>', reject_request, name='reject_request'),
+    path('request-book-mobile', request_book_by_ajax, name='request_book_mobile'),
 ]
+
+# error DBnya
+# bisa benerinnya?
+# ngecas kok ga masuk itu 
+# kalau DBnya kayaknya perlu sama koor asdosnya pak soalnya itu di masalah deployment :(
--- book_request/views.py
+++ book_request/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.admin.views.decorators import staff_member_required
-from django.http import JsonResponse, HttpResponse, HttpResponseForbidden
+from django.http import JsonResponse, HttpResponse, HttpResponseForbidden, HttpResponseNotFound
 from django.core import serializers
 from .models import Book, RequestBook
 from django.views.decorators.csrf import csrf_exempt
@@ -25,6 +25,43 @@ def request_book_by_ajax(request):
         form = RequestBookForm()
         return render(request, 'book_request_by_ajax.html', {'form': form})
 
+# @csrf_exempt
+# def request_book_by_ajax(request):
+#     if request.method == 'POST':
+#         name = request.POST.get("name")
+#         author = request.POST.get("author")
+#         original_language = request.POST.get("original_language")
+#         year_published = request.POST.get("year_published")
+#         sales = request.POST.get("sales")
+#         genre = request.POST.get("genre")
+#         cover_image = request.POST.get("cover_image")
+#         user = request.user
+#         request_status = "PENDING"
+#         new_book_request = RequestBook(name=name, author=author, original_language=original_language,
+#         year_published=year_published, sales=sales, genre=genre, cover_image=cover_image, user=user, request_status=request_status)
+#         new_book_request.save()
+#         return HttpResponse(b"CREATED REQUEST", status=201)
+#     return HttpResponseNotFound()
+
+@csrf_exempt
+def request_book_mobile(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        new_book_request = RequestBook.objects.create(
+            user = request.user,
+            name = data['name'],
+            author = data['author'],
+            original_language = data['original_language'],
+            year_published = int(data['year_published']),
+            sales = int(data['sales']),
+            genre = data['genre'],
+            cover_image = data['cover_image']
+        )
+        new_book_request.save()
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+    
 @login_required
 def get_requested_book_json(request):
     data = RequestBook.objects.filter(user=request.user)
@@ -55,7 +92,7 @@ def approve_request(request, request_id):
             year_published = data["year_published"],
             sales = data["sales"],
             genre = data["genre"],
-            # cover_image = data["cover_image"]
+            cover_image = data["cover_image"]
         )
         new_book.save()
 
@@ -67,9 +104,9 @@ def load_books():
     with open('main:fixtures/initial_books.json', 'r') as file:
         return json.load(file)
         
-@login_required
+@csrf_exempt
 def reject_request(request, request_id):
-    if not request.user.is_staff:
+    if not request.user.is_superuser:
         return HttpResponseForbidden("You don't have permission to perform this action.")
     
     req = get_object_or_404(RequestBook, pk=request_id)
